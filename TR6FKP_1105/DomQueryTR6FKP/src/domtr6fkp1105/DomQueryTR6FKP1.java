2acabffc9f91877d5716ce437e81124a5daa4a8e
PogiAbel <pogonyiabel@gmail.com>
2024-11-05 14:00:12 +0100

    task2 b,c

--- TR6FKP_1105/DomQueryTR6FKP/src/domtr6fkp1105/DomQueryTR6FKP1.java
+++ TR6FKP_1105/DomQueryTR6FKP/src/domtr6fkp1105/DomQueryTR6FKP1.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -20,7 +24,9 @@ public class DomQueryTR6FKP1 {
 
         Document doc = builder.parse("./TR6FKP_orarend.xml");
 
-        taskA(doc);
+        // taskA(doc);
+        taskB(doc);
+        // taskC(doc);
 
     }
 
@@ -37,6 +43,43 @@ public class DomQueryTR6FKP1 {
         }
 
         System.out.println(Arrays.toString(kurzusokList));
+    }
+
+    public static void taskB(Document doc) throws Exception {
+        Node root = doc.getDocumentElement();
+        NodeList childs = root.getChildNodes();
+
+        Node first = root;
+
+        for(int i = 0; i < childs.getLength(); i++){
+            Node child = childs.item(i);
+            if(child.getNodeType() == Node.ELEMENT_NODE){
+                first = child;
+                break;
+            }
+        }
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transf = transformerFactory.newTransformer();
+
+        DOMSource source = new DOMSource(first);
+
+        StreamResult console = new StreamResult(System.out);
+        // StreamResult file = new StreamResult("./orarendModifyTR6FKP.xml");
+
+        transf.transform(source, console);
+        // transf.transform(source, file);
+    }
+
+    public static void taskC(Document doc) {
+
+        NodeList oktatok = doc.getElementsByTagName("oktato");
+        String[] nevek = new String[oktatok.getLength()]; 
+        for (int i = 0; i < oktatok.getLength(); i++) {
+            nevek[i] = oktatok.item(i).getTextContent();
+        }
+
+        System.out.println(Arrays.toString(nevek));
     }
     
 }
